b7970294f5b1a23183f0326da8a85da63b624c4b
IssacDesigner <wangc9922@gmail.com>
2025-02-17 04:06:40 +0800

    bago ito

---
+++ index1.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/index.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  <!-- Google Material Icons Outlined -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <title>High Twelve</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-navbar col-lg-12 col-md-12 col-sm-12">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <h2 class="navbar-brand">
+              <span class="spanhigh">H</span>IGH
+              <span class="spanhigh">T</span>WELVE
+            </h2>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link" href="#">HOME</a>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">ABOUT FREEMANSONRY</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">WHAT IS FREEMANSONRY?</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">BECOME A MEMBER</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">FREQUENTLY ASKED</a>
+                    </li>
+                    <li><a class="dropdown-item" href="#">QUESTIONS</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">THE GRAND LODGE</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">ABOUT THE GRAND LODGE</a>
+                    </li>
+                    <li><a class="dropdown-item" href="#">LODGES</a></li>
+                    <li>
+                      <a class="dropdown-item" href="#">PAST GRAND MASTERS</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">FOREIGN GRAND LODGES</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">CHARITIES</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">DONATE</a>
+                    </li>
+                    <li><a class="dropdown-item" href="#">OFFICES</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">RESOURCES</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#"></a>
+                    </li>
+                    <li><a class="dropdown-item" href="#"></a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false">CONTACT</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">DIRECTORY</a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">LODGE LOCATOR</a>
+                    </li>
+                  </ul>
+                </li>
+                <button id="btn-login" class="btn-login" type="button">
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="col-company-logo col-lg-12 col-md-12 col-sm-12">
+        <img src="img/logo.png" alt="" />
+        <h2>
+          The Most Worshipful Grand Lodge of Free and Accepted Masons of the
+          Philippines
+        </h2>
+      </div>
+      <div class="col-img col-lg-12 col-md-12 col-sm-12">
+        <img src="img/Screenshot 2025-02-11 091749.png" alt="" />
+      </div>
+      <div class="col-company-quotes col-lg-12 col-md-12 col-sm-12">
+        <div class="quotes">
+          <a href="#">"Let There Be More Men in the Light, and Let There Be More Light
+            Among Masons"</a>
+          <img src="img/q.png" alt="" />
+        </div>
+      </div>
+      <div class="col-masters-act col-lg-12 col-md-12 col-sm-12">
+        <h2>GRAND MASTER'S ACTIVITIES</h2>
+        <div class="acts">
+          <div class="act">
+            <img src="img/4.jpg" alt="" />
+            <a href="#">Lodge Dedication of WB Manuel C Ibasco Masonic Lodge No. 432,
+              September 25, 2024</a>
+          </div>
+          <div class="act">
+            <img src="img/t.jpg" alt="" />
+            <a href="#">A Tribute to Marcelo H. Del Pilar: A Freemasonry Perspective</a>
+            <p>
+              A notable event took place in Bulakan, Bulacan, as Most
+              Worshipful Ariel T Cayanan, the Grand Master of the Masons of
+              the Philippines, attended the commemoration of Marcelo H. Del
+              Pilar.
+            </p>
+          </div>
+        </div>
+        <div class="other-acts">
+          <a href="#">Unveiling of Pacific Masonic Lodge No. 501</a>
+          <a href="#">Flag Raising Ceremony for the month of August 2024 hosted by MD
+            R-V Camarines Norte, Camarines Sur and Catanduanes, and Supreme
+            Council Order of DeMolay in Coordination with Masonic Youth</a>
+          <a href="#">Past Masters Night of Mt. Mainam Masonic Lodge No. 49, July 20,
+            2024</a>
+          <a href="#">106th Founding Anniversary of Pampanga Masonic Lodge No. 48, July
+            20, 2024</a>
+          <a href="#">Columbian Lodge No. 484 New York, proudly celebrated the 165th
+            Installation of Officers, graced by the esteemed presence of our
+            Grand Master, MW Ariel T. Cayanan.</a>
+          <a href="#">More updates...</a>
+        </div>
+      </div>
+      <div class="col-officer col-lg-12 col-md-12 col-sm-12">
+        <h2>GRAND LODGE OFFICERS</h2>
+        <div class="swiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/MW_Cayanan_Temp__website-removebg-preview_1.png" alt="" />
+                <h3>MW Ariel T. Cayanan</h3>
+                <p>Grand Master</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/Raul_Canon.png" alt="" />
+                <h3>RW Raul E. Canon, Jr.</h3>
+                <p>Deputy Grand Master</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/RW Bob Moran_1_0.jpg" alt="" />
+                <h3>RW Robert Joseph S. Moran</h3>
+                <p>Senior Grand Warden</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/Ariel_S._Rivera-removebg-preview.png" alt="" />
+                <h3>RW Ruel S. Rivera</h3>
+                <p>Junior Grand Warden</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/PGM-Espino3-removebg-preview.png" alt="" />
+                <h3>MW Juanito G. Espino, Jr., PGM</h3>
+                <p>Grand Treasurer</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-officers">
+                <img src="img/MW_ANGELES-removebg-preview.png" alt="" />
+                <h3>MW Danilo D. Angeles, PGM</h3>
+                <p>Grand Secretary</p>
+              </div>
+            </div>
+          </div>
+          <!-- Pagination and navigation -->
+          <div class="btn-arrow swiper-pagination"></div>
+          <div class="btn-arrow swiper-button-next"></div>
+          <div class="btn-arrow swiper-button-prev"></div>
+        </div>
+      </div>
+      <div class="col-allied-org col-lg-12 col-md-12 col-sm-12">
+        <h2>APPENDANT BODIES AND ALLIED ORGANIZATION</h2>
+        <div class="swiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/Scottish_Rite_Double_Headed_Eagle.gif" alt="" />
+                <a href="#">Scottish Rite</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/york.png" alt="" />
+                <a href="#">York Rite</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/AmaranthLogo.gif" alt="" />
+                <a href="#">Order of the Amaranth</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/oes_emblem_2.jpg" alt="" />
+                <a href="#">Order of the Eastern Star</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/demolay.jpg" alt="" />
+                <a href="#">DeMolay International</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/rainbow.png" alt="" />
+                <a href="#">Rainbow for Girls</a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/jobs_daughters.png" alt="" />
+                <a href="#">Job's Daughters Shriners </a>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-allied">
+                <img src="img/shriners.png" alt="" />
+                <a href="#">Shriners International</a>
+              </div>
+            </div>
+          </div>
+          <!-- Pagination and navigation -->
+          <div class="btn-arrow swiper-pagination"></div>
+          <div class="btn-arrow swiper-button-next"></div>
+          <div class="btn-arrow swiper-button-prev"></div>
+        </div>
+      </div>
+      <div class="col-grand col-lg-12 col-md-12 col-sm-12">
+        <img class="grandimg" src="img/MW_Cayanan_Temp__website-removebg-preview_1.png" alt="" />
+        <div class="info">
+          <a href="#">"Let There Be More Men in the Light, and Let There Be More Light
+            Among Masons"</a>
+          <h2>MW Ariel T. Cayanan</h2>
+          <p>Grand Master of Masons Masonic Year 6024-6025 A.L.</p>
+        </div>
+        <img class="grandlogo" src="img/MW_Cayanan_Logo_page-0001-removebg-preview.png" alt="" />
+      </div>
+      <div class="col-footer col-lg-12 col-md-12 col-sm-12">
+        <div class="footer-cards">
+          <div class="cards">
+            <h2>For Members</h2>
+            <hr />
+            <a href="">Member Login</a>
+            <a href="">Edicts</a>
+            <a href="">GM Circulars</a>
+            <a href="">GS Circulars</a>
+            <a href="">The Cable Tow</a>
+          </div>
+          <div class="cards">
+            <h2>Quicklinks</h2>
+            <hr />
+            <a href="">Becoming a Freemason</a>
+            <a href="">Lodge Locator</a>
+            <a href="">Grand Lodge Directory</a>
+            <a href="">Events</a>
+            <a href="">Foreign Grand Lodges</a>
+            <a href="">Disaster and Emergency</a>
+          </div>
+          <div class="cards">
+            <h2>Events</h2>
+            <hr />
+          </div>
+          <div class="cards">
+            <h2>Contact Us</h2>
+            <hr />
+          </div>
+        </div>
+        <hr />
+        <div class="copyright">
+          <small>
+            The Grand Jurisdiction of the Most Worshipful Grand Lodge of F. &
+            A.M. of the Philippines comprises the territory of the
+            Philippines, South Korea, the Marianas Islands, the Marshal
+            Islands, the Ryukyu Islands, South Vietnam and its existing Lodges
+            in Japan.
+          </small>
+          <small>Copyright © 2019-2024. All rights reserved.</small>
+          <small>New Plaridel Masonic Temple, 1440 San Marcelino Street, Ermita,
+            Manila 1000 Philippines</small>
+        </div>
+      </div>
+      <div id="switchmode" class="switchmode">
+        <span class="light material-icons-outlined"> light_mode </span>
+        <span class="dark material-icons-outlined"> dark_mode </span>
+      </div>
+    </div>
+  </div>
+  <div class="login">
+    <div class="container">
+      <div id="to-content" class="arrowback">
+        <span class="material-icons-outlined">
+          cancel
+        </span>
+      </div>
+      <div class="rowlogin row">
+        <div class="col-lg-6 col-md-6 col-sm-12 container-logo-name">
+          <div class="logo">
+            <img src="img/logo.png" alt="" />
+            <div class="name">
+              <h2>HIGH TWELVE 82</h2>
+              The Most Worshipful Grand Lodge of Free and Accepted Masons of
+              the Philippines
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-signin-signup mt-5 mx-auto">
+          <div class="signin">
+            <h2>Login</h2>
+            <span>Input the needed details to log in</span>
+            <form id="form-login">
+              <input id="login-email" name="login-email" class="form-control" type="text" placeholder="Email Address or Username" required />
+              <input id="login-password" name="login-password" class="form-control" type="password" placeholder="Password" required />
+              <button type="submit" class="signin-btn">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/index.js"></script>
+</body>
+
+</html>
---
+++ js/index1.js
@@ -0,0 +1,118 @@
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    428: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+
+let lightmode = localStorage.getItem("lightmode");
+const switchmode = document.getElementById("switchmode");
+
+const enablelightmode = () => {
+  document.body.classList.add("lightmode");
+  localStorage.setItem("lightmode", "active");
+};
+
+const disablelightmode = () => {
+  document.body.classList.remove("lightmode");
+  localStorage.setItem("lightmode", null);
+};
+
+if (lightmode === "active") enablelightmode();
+
+switchmode.addEventListener("click", () => {
+  lightmode = localStorage.getItem("lightmode");
+  lightmode !== "active" ? enablelightmode() : disablelightmode();
+});
+
+$(document).ready(function () {
+  $("#btn-login").on("click", function () {
+    $(".login").addClass("actives");
+    $("body").css("overflow", "hidden");
+  });
+  $("#to-content").on("click", function () {
+    $(".login").removeClass("actives");
+    $("body").css("overflow", "");
+  });
+
+  $("#to-signup").on("click", function () {
+    $(".signup").addClass("active");
+    $(".signin").addClass("active");
+  });
+
+  $("#to-signin").on("click", function () {
+    $(".signup").removeClass("active");
+    $(".signin").removeClass("active");
+  });
+
+  $("#form-login").submit(function (event) {
+    event.preventDefault();
+
+    var formData = new FormData(this);
+
+    $.ajax({
+      url: "php/Login.php",
+      method: "POST",
+      data: formData,
+      dataType: "json",
+      contentType: false,
+      processData: false,
+      success: function (response) {
+        if (response.success) {
+          Swal.fire({
+            icon: "success",
+            title: "Login Successful!",
+            text: "Welcome, " + response.position + "!",
+          }).then(() => {
+            if (response.position === "Admin") {
+              window.location.href = "admin.php";
+            }
+          });
+        } else {
+          let errorMessage = response.message;
+
+          if (errorMessage.includes("Invalid Password")) {
+            errorMessage = "Incorrect password. Please try again.";
+          } else if (
+            errorMessage.includes("Invalid Email") ||
+            errorMessage.includes("Invalid Email or Username")
+          ) {
+            errorMessage = "Invalid Email or Username. Please try again.";
+          }
+
+          Swal.fire({
+            icon: "error",
+            title: "Login Failed",
+            text: errorMessage,
+          });
+        }
+      },
+      error: function () {
+        Swal.fire({
+          icon: "error",
+          title: "Request Failed",
+          text: "There was a problem with the server or network. Please try again.",
+        });
+      },
+    });
+  });
+});
